9f2348834fcece358dccded4170c5fd291be2cde
20171105135 <1115828778@qq.com>
2018-06-29 08:39:45 +0800

    Initial Commit

---
+++ 评委打分2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  评委打分2
+//
+//  Created by 王皎 on 18/6/29.
+//  Copyright © 2018年 王皎. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
